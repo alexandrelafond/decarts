99778fbd4afa2a5a8d0e35ff2950213626157840
Alexandre Lafond <a.lafond@agendrix.com>
2020-11-21 10:15:25 -0500

    Added tailwind

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,24 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header className="px-4 py-6 sm:px-6">
+    <div className="flex items-center">
+      <h1>
+        <Link to="/">{siteTitle}</Link>
       </h1>
+      <div className="flex flex-1 justify-between">
+        <div className="flex ml-12 space-x-4">
+          <Link to="/about/" className="hover:underline">
+            À propos
+          </Link>
+        </div>
+        <a
+          className="underline font-bold"
+          href={"mailto:" + "jacquesdespres@sympatico.ca"}
+        >
+          Nous écrire
+        </a>
+      </div>
     </div>
   </header>
 )
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="about" />
+    <div>Page a propos</div>
+  </Layout>
+)
+
+export default About
