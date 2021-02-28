4ce2aa90099d8a38cc551671696bb1dafc59b147
Alexandre Lafond <a.lafond@agendrix.com>
2021-02-27 22:45:51 -0500

    Rollback displayed info in gallery

--- src/components/item.js
+++ src/components/item.js
@@ -3,21 +3,17 @@ import React from "react"
 import Img from "gatsby-image"
 
 const Item = ({ file, fileInfo }) => {
-  return fileInfo?.sold === false ? (
-    <li key={file.id} className="text-center">
-      <Link to={`gallery/${file.id}`}>
-        {fileInfo && (
-          <>
-            <h3>{`${fileInfo.artist.name} (${fileInfo.artist.origin})`}</h3>
-            <p>{`${fileInfo.info.title} (${fileInfo.info.year})`}</p>
-          </>
-        )}
-        {!fileInfo && <h3>{file.name}</h3>}
-        <Img fixed={file.childImageSharp.fixed} />
-      </Link>
-    </li>
-  ) : (
-    <></>
+  return (
+    <>
+      {fileInfo && fileInfo.sold === false && (
+        <li key={file.id} className="text-center">
+          <Link to={`gallery/${file.id}`}>
+            <h3>{file.name}</h3>
+            <Img fixed={file.childImageSharp.fixed} />
+          </Link>
+        </li>
+      )}
+    </>
   )
 }
 
